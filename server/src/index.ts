9ad6f84c05b848927604ce2b69937f30b9dc594b
Fabian Trinler <fabian.trinler@blateral.com>
2021-12-07 22:26:57 +0100

    Adding graphql-yoga

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,21 @@
-export default () => {
-    console.log('hey');
+require('dotenv').config();
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+    Query: {
+        hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+    },
 };
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start({ port: process.env.GRAPHQL_SERVER_PORT }, () =>
+    console.log(
+        `Server is running on localhost:${process.env.GRAPHQL_SERVER_PORT}`
+    )
+);
