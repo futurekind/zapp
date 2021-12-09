7dd6353ee703aacec56732fcb5cfdeea6770c6b3
Fabian Trinler <fabian.trinler@blateral.com>
2021-12-09 22:50:20 +0100

    Adding jwt token generation

--- server/prisma/resolvers/LoginResolver.ts
+++ server/prisma/resolvers/LoginResolver.ts
@@ -1,13 +1,34 @@
-import { Arg, Mutation, Resolver } from 'type-graphql';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import { AppContext } from '../../utils/types';
 
 @Resolver()
 class LoginResolver {
     @Mutation((returns) => String!)
     async login(
+        @Ctx() { prisma }: AppContext,
         @Arg('email') email: string,
         @Arg('password') password: string
     ) {
-        return new Promise((res) => res('hey'));
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) throw new Error('Wrong login credentials');
+
+        const passwordsMatch = await compare(password, user.password);
+
+        if (!passwordsMatch) throw new Error('Wrong login credentials');
+
+        prisma.user.update({
+            where: { id: user.id },
+            data: { lastLoginAt: new Date() },
+        });
+
+        const token = sign({ id: user.id }, process.env.JWT_SECRET as string);
+
+        return token;
     }
 }
 
