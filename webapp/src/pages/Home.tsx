6db601d46c9eeaa96b45092833c9af747b351c4a
Fabian Trinler <fabian.trinler@blateral.com>
2021-12-13 07:51:12 +0100

    Adding basic page template to home

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,8 +1,16 @@
 import { FC } from 'react';
 import { ProtectedPage, withProtection } from 'utils/withProtection';
+import { EuiPageTemplate } from '@elastic/eui';
 
 const Home: FC<ProtectedPage> = ({ me }) => {
-    return <span>Hey {me.firstname}</span>;
+    return (
+        <EuiPageTemplate
+            template="centeredBody"
+            pageContentProps={{ paddingSize: 'none' }}
+        >
+            <span>Hey {me.firstname}</span>
+        </EuiPageTemplate>
+    );
 };
 
 export default withProtection(Home);
